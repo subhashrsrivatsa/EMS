f1da0d57e300949f56e43de36f233a0f7e66112a
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2021-06-03 22:02:32 +0200

    added employee model and repo

---
+++ ems/src/main/java/com/ems/model/Employee.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.ems.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Subhash
+ *
+ */
+
+@Entity
+@Table(name = "employees")
+public class Employee 
+{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	/**
+	 * 
+	 */
+	public Employee() {
+		
+	}
+	
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param emailId
+	 */
+	public Employee(String firstName, String lastName, String emailId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+	/**
+	 * @param emailId the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+}
---
+++ ems/src/main/java/com/ems/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ems.model.Employee;
+
+/**
+ * @author Subhash
+ *
+ */
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>
+{
+
+}
