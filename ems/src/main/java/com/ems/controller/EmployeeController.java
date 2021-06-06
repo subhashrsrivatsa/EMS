5aa0389a111e5721fd25f4f8aaa512b037dc3bec
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2021-06-06 16:44:23 +0200

    added create-employee REST API

--- ems/src/main/java/com/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/ems/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,11 @@ public class EmployeeController
 	public List<Employee> getAllEmployees()
 	{
 		return employeeRepository.findAll();
+	}
+	
+	// create employee REST API
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
