482c12c8837ac3eb3a5bb61291a338f314193b33
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2021-06-06 22:04:10 +0200

    created REST API to get single Employee object

--- ems/src/main/java/com/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/ems/controller/EmployeeController.java
@@ -6,12 +6,15 @@ package com.ems.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ems.exception.ResourceNotFoundException;
 import com.ems.model.Employee;
 import com.ems.repository.EmployeeRepository;
 
@@ -39,5 +42,16 @@ public class EmployeeController
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	// get employee by id REST API
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee>  getEmployeeById(@PathVariable Long id) {
+		
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id : "+id));
+		
+		return ResponseEntity.ok(employee);
+		 
 	}
 }
