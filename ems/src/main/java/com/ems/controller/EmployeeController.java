af1d9ece9a9ecbd714edaa4af2d9f931ec80bc05
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2021-06-06 22:13:25 +0200

    update employee REST API

--- ems/src/main/java/com/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/ems/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +52,19 @@ public class EmployeeController
 		Employee employee = employeeRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id : "+id));
 		
-		return ResponseEntity.ok(employee);
-		 
+		return ResponseEntity.ok(employee);	 
+	}
+	
+	//update employee REST API
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id : "+id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedEmployee =  employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
 	}
 }
